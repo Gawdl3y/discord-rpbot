5a065eb67fa1294ad0101ea2ad86ae83b3fd7629
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-16 18:27:13 -0400

    Added less than functionality to rolls

--- src/commands/dice-roll.js
+++ src/commands/dice-roll.js
@@ -6,8 +6,8 @@ import DiceExpression from 'dice-expression-evaluator';
 export default class DiceRollCommand {
 	constructor() {
 		this.matches = [
-			/^!roll\s+([0-9d+ -]+)(>\s*([0-9]+))?\s*$/i,
-			/\(\s*roll:\s*([0-9d+ -]+)(>\s*([0-9]+))?\s*\)/i
+			/^!roll\s+([0-9d+ -]+)((>|<)\s*([0-9]+))?\s*$/i,
+			/\(\s*roll:\s*([0-9d+ -]+)((>|<)\s*([0-9]+))?\s*\)/i
 		];
 	}
 
@@ -17,8 +17,18 @@ export default class DiceRollCommand {
 			const result = dice.roll();
 			console.log(result);
 			if(matchResult[3]) {
-				const target = parseInt(matchResult[3]);
-				message.client.sendMessage(message, message.author + (target <= result.roll ? ' has **succeeded**. ' : ' has **failed**. ') + ' (Rolled ' + result.roll + ', target ' + target + ')');
+				const operator = matchResult[3];
+				const target = parseInt(matchResult[4]);
+				let success = false;
+				let targetMessage = '';
+				if(operator === '>') {
+					success = result.roll > target;
+					targetMessage = (!success ? 'not ' : '') + 'greater than ' + target;
+				} else {
+					success = result.roll < target;
+					targetMessage = (!success ? 'not ' : '') + 'less than ' + target;
+				}
+				message.client.sendMessage(message, message.author + (success ? ' has **succeeded**. ' : ' has **failed**. ') + ' (Rolled ' + result.roll + ', ' + targetMessage + ')');
 			} else {
 				message.client.sendMessage(message, message.author + ' rolled **' + result.roll + '**.');
 			}
