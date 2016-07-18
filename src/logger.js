c460140bc06deb405aebace4a032abf528424f71
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-17 20:31:47 -0400

    Fixed instantiation of transports

--- src/logger.js
+++ src/logger.js
@@ -6,7 +6,7 @@ import config from './config';
 
 const logger = new winston.Logger({
 	transports: [
-		new winston.transports.Console({
+		new (winston.transports.Console)({
 			colorize: true,
 			timestamp: true
 		})
@@ -14,11 +14,11 @@ const logger = new winston.Logger({
 });
 
 if(config.log) {
-	logger.add(new winston.transports.File({
+	logger.add(winston.transports.File, {
 		filename: config.log,
 		level: config.logLevel,
 		json: false
-	}));
+	});
 }
 
 export default logger;
