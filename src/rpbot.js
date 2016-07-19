407f7f02f24dccd6c2d437826c4efea94f0066df
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-19 19:00:00 -0400

    Prepare 1.2.0 release

--- src/rpbot.js
+++ src/rpbot.js
@@ -7,7 +7,7 @@ import config from './config';
 import logger from './logger';
 
 // Version info
-export const VERSION = { code: 10102, string: '1.1.2' };
+export const VERSION = { code: 10200, string: '1.2.0' };
 
 // Commands
 import HelpCommand from './commands/help';
