cc38830f09097ee0a1079d778540cc15f35b7c36
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-22 03:04:31 -0400

    Improved paginator behaviour

--- src/util/pagination.js
+++ src/util/pagination.js
@@ -2,10 +2,10 @@
 'use strict';
 
 export default function paginate(items, page, pageLength = 15) {
-	if(page < 1) page = 1;
-	let startIndex = (page - 1) * pageLength;
-	if(startIndex > items.length) { startIndex = 0; page = 1; }
 	const maxPage = Math.ceil(items.length / pageLength);
+	if(page < 1) page = 1;
+	if(page > maxPage) page = maxPage;
+	let startIndex = (page - 1) * pageLength;
 	return {
 		items: items.length > pageLength ? items.slice(startIndex, startIndex + pageLength) : items,
 		page: page,
