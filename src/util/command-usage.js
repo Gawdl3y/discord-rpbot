4184c0fbe7d10b3fd529f81e2458bb064101357f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-30 19:16:25 -0400

    Added a space to command usages if desirable

--- src/util/command-usage.js
+++ src/util/command-usage.js
@@ -8,7 +8,8 @@ import nbsp from './nbsp';
 
 export function long(command, server = null) {
 	const nbcmd = nbsp(command);
-	const prefix = nbsp(server ? SettingsDatabase.getSettingValue('command-prefix', config.commandPrefix, server) : config.commandPrefix);
+	let prefix = nbsp(server ? SettingsDatabase.getSettingValue('command-prefix', config.commandPrefix, server) : config.commandPrefix);
+	if(prefix.length > 1) prefix += '\xa0';
 	const prefixAddon = prefix ? `\`${prefix}${nbcmd}\` or ` : '';
 	return `${prefixAddon}\`@${nbsp(client.user.name)}#${client.user.discriminator}\xa0${nbcmd}\``;
 }
