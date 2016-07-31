909f50819552aed36b3e5bbcd16ebcaa4b298ba1
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-31 01:24:34 -0400

    Added usage statistics to about

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -16,7 +16,13 @@ export default {
 
 	run(message) {
 		const owner = message.client.users.get('id', config.owner);
-		const ownerAddon = owner ? `\n**Bot owner:** ${owner.name}#${owner.discriminator}` : '';
-		message.client.reply(message, `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y). https://github.com/Gawdl3y/discord-rpbot${ownerAddon}`);
+		const ownerAddon = owner ? `owned by ${owner.name}#${owner.discriminator}, and ` : '';
+		const serverCount = message.client.servers.length;
+		const userCount = message.client.users.length;
+		const serverLabel = 'server' + (serverCount > 1 || serverCount === 0 ? 's' : '');
+		const userLabel = 'user' + (userCount > 1 || userCount === 0 ? 's' : '');
+		const general = `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y). https://github.com/Gawdl3y/discord-rpbot`;
+		const usage = `This bot ${ownerAddon}is serving ${userCount} ${userLabel} across ${serverCount} ${serverLabel}.`;
+		message.client.reply(message, `${general}\n${usage}`);
 	}
 };
