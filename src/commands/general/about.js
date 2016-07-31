f2a2c67647c1a4808d1a639c46e87d277978c790
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-07-31 19:08:00 -0400

    Cleaned up about response code

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -1,6 +1,7 @@
 'use babel';
 'use strict';
 
+import { stripIndents } from 'common-tags';
 import config from '../../config';
 import version from '../../version';
 
@@ -16,13 +17,13 @@ export default {
 
 	run(message) {
 		const owner = message.client.users.get('id', config.owner);
-		const ownerAddon = owner ? `is owned by ${owner.name}#${owner.discriminator}, and ` : '';
 		const serverCount = message.client.servers.length;
 		const userCount = message.client.users.length;
 		const serverLabel = 'server' + (serverCount > 1 || serverCount === 0 ? 's' : '');
 		const userLabel = 'user' + (userCount > 1 || userCount === 0 ? 's' : '');
-		const general = `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y). https://github.com/Gawdl3y/discord-rpbot`;
-		const usage = `This bot ${ownerAddon}is serving ${userCount} ${userLabel} across ${serverCount} ${serverLabel}.`;
-		message.reply(`${general}\n${usage}`);
+		message.reply(stripIndents`
+			**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y). https://github.com/Gawdl3y/discord-rpbot
+			This bot ${owner ? `is owned by ${owner.name}#${owner.discriminator}, and ` : ''}is serving ${userCount} ${userLabel} across ${serverCount} ${serverLabel}.
+		`);
 	}
 };
