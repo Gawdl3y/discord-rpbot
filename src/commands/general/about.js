6dcc77c48bc2f0e5dcdc6764e8dd7edd2f5508c5
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-01 14:37:58 -0400

    Added uptime to about command

--- src/commands/general/about.js
+++ src/commands/general/about.js
@@ -17,13 +17,15 @@ export default {
 
 	run(message) {
 		const owner = message.client.users.get('id', config.owner);
-		const serverCount = message.client.servers.length;
-		const userCount = message.client.users.length;
-		const serverLabel = 'server' + (serverCount > 1 || serverCount === 0 ? 's' : '');
-		const userLabel = 'user' + (userCount > 1 || userCount === 0 ? 's' : '');
+		const servers = message.client.servers.length.toLocaleString(), users = message.client.users.length.toLocaleString();
+		const serversLabel = servers != 1 ? 'servers' : 'server', usersLabel = users != 1 ? 'users' : 'user';
+		const uptime = process.uptime();
+		const days = Math.floor(uptime / 60 / 60 / 24).toLocaleString(), hours = Math.floor(uptime / 60 / 60), minutes = Math.floor(uptime / 60);
+		const daysLabel = days != 1 ? 'days' : 'day', hoursLabel = hours != 1 ? 'hours' : 'hour', minutesLabel = minutes != 1 ? 'minutes' : 'minute';
 		message.reply(stripIndents`
 			**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y). https://github.com/Gawdl3y/discord-rpbot
-			This bot ${owner ? `is owned by ${owner.name}#${owner.discriminator}, and ` : ''}is serving ${userCount} ${userLabel} across ${serverCount} ${serverLabel}.
+			This bot ${owner ? `is owned by ${owner.name}#${owner.discriminator}, and ` : ''}is serving ${users} ${usersLabel} across ${servers} ${serversLabel}.
+			It has been running without interruption for ${days} ${daysLabel} ${hours} ${hoursLabel} ${minutes} ${minutesLabel}.
 		`);
 	}
 };
