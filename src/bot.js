ac976f04ea7918818eb1eef503794ba7bd8747b9
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-17 01:52:21 -0400

    Disabled everyone

--- src/bot.js
+++ src/bot.js
@@ -8,5 +8,6 @@ export default new GrafBot({
 	botName: 'RPBot',
 	botVersion: version,
 	botAbout: `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y).\nSource code and information: https://github.com/Gawdl3y/discord-rpbot`,
-	botUpdateURL: 'https://raw.githubusercontent.com/Gawdl3y/discord-rpbot/master/package.json'
+	botUpdateURL: 'https://raw.githubusercontent.com/Gawdl3y/discord-rpbot/master/package.json',
+	disableEveryone: true
 });
