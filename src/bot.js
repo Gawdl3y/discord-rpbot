f545149df4c585f02ac025fcd16c2d5810f45254
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-08-22 22:30:05 -0400

    Updated to GRAF 0.4.0

--- src/bot.js
+++ src/bot.js
@@ -5,9 +5,9 @@ import GrafBot from 'discord-graf';
 import version from './version';
 
 export default new GrafBot({
-	botName: 'RPBot',
-	botVersion: version,
-	botAbout: `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y).\nSource code and information: https://github.com/Gawdl3y/discord-rpbot`,
-	botUpdateURL: 'https://raw.githubusercontent.com/Gawdl3y/discord-rpbot/master/package.json',
+	name: 'RPBot',
+	version: version,
+	about: `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y).\nSource code and information: https://github.com/Gawdl3y/discord-rpbot`,
+	updateURL: 'https://raw.githubusercontent.com/Gawdl3y/discord-rpbot/master/package.json',
 	clientOptions: { disableEveryone: true }
 });
