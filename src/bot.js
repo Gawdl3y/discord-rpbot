7b2c6373eab0a35a39fd45290232718693e3d496
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-09-16 03:42:13 -0400

    Cleaned up bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,13 +1,17 @@
 'use babel';
 'use strict';
 
-import GrafBot from 'discord-graf';
+import { Bot } from 'discord-graf';
+import { stripIndents } from 'common-tags';
 import version from './version';
 
-export default new GrafBot({
+export default new Bot({
 	name: 'RPBot',
 	version: version,
-	about: `**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y).\nSource code and information: https://github.com/Gawdl3y/discord-rpbot`,
+	about: stripIndents`
+		**RPBot** v${version} created by Schuyler Cebulskie (Gawdl3y).
+		Source code and information: https://github.com/Gawdl3y/discord-rpbot
+	`,
 	updateURL: 'https://raw.githubusercontent.com/Gawdl3y/discord-rpbot/master/package.json',
-	clientOptions: { disableEveryone: true }
+	clientOptions: { disable_everyone: true } // eslint-disable-line camelcase
 });
